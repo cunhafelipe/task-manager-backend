9339aeec84178a21090d3a7cafb2c19b171b6f2d
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-07-02 19:54:03 -0300

    feat: add route to get a task by id

--- index.js
+++ index.js
@@ -20,6 +20,20 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+app.get("tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await taskModel.findById(taskId);
+
+        if (!task) {
+            return res.status(404).send("Task not found");
+        }
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new taskModel(req.body);
@@ -37,7 +51,7 @@ app.delete("/tasks/:id", async (req, res) => {
         const taskToDelete = await taskModel.findById(taksId);
 
         if (!taskToDelete) {
-            return res.status(500).send("Task not found");
+            return res.status(404).send("Task not found");
         }
         const deleteTask = await taskModel.findByIdAndDelete(taksId);
 
